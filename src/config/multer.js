a8d02b56a5ae8ef13fdd3ac4c307130fab217408
Bruno Rossini <brunorossini@live.com>
2020-03-26 13:03:29 -0300

    feat(multer config to send files to spaces): multer config to send files to spaces digital ocean

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,14 +1,24 @@
-import multer from 'multer';
+import AWS from 'aws-sdk';
 import crypto from 'crypto';
-import { extname, resolve } from 'path';
+import multerS3 from 'multer-s3';
+import { extname } from 'path';
+
+const spacesEndpoint = new AWS.Endpoint(process.env.SPACES_ENDPOINT);
+
+const s3 = new AWS.S3({
+  endpoint: spacesEndpoint,
+  accessKeyId: process.env.SPACES_KEY,
+  secretAccessKey: process.env.SPACES_SECRET,
+});
 
 export default {
-  storage: multer.diskStorage({
-    destination: resolve(__dirname, '..', '..', 'temp', 'uploads'),
-    filename: (req, file, cb) => {
+  storage: multerS3({
+    s3,
+    bucket: process.env.SPACES_BUCKET,
+    acl: 'public-read',
+    key: (req, file, cb) => {
       crypto.randomBytes(16, (err, res) => {
         if (err) return cb(err);
-
         return cb(null, res.toString('hex') + extname(file.originalname));
       });
     },
